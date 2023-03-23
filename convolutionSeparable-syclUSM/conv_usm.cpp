195a734b727d2d4389cebea122db7de40b90329d
Martin <martin.wehking@codeplay.com>
2023-03-23 16:38:53 +0000

    Add functor conv rows kernel

--- convolutionSeparable-syclUSM/conv_usm.cpp
+++ convolutionSeparable-syclUSM/conv_usm.cpp
@@ -10,9 +10,14 @@
  */
 
 #include <assert.h>
-#include <CL/sycl.hpp>
+#include <sycl/sycl.hpp>
+#include <sycl/ext/oneapi/experimental/cuda/builtins.hpp>
 #include "common.h"
 #include "conv.h"
+
+using namespace ext::oneapi::experimental::cuda;
+
+// #include <sycl/ext/oneapi/experimental/cuda/builtins.hpp>
 
 #define ROWS_BLOCKDIM_X 16
 #define COLUMNS_BLOCKDIM_X 16
@@ -23,9 +28,77 @@
 #define ROWS_HALO_STEPS 1
 #define COLUMNS_HALO_STEPS 1
 
+class ConvRows {
+    public:
+
+        ConvRows(
+            float* __restrict__ d_D,
+            const float *__restrict__ d_S,   
+            const float *__restrict__ d_K, 
+            accessor<float, 2, sycl_read_write, access::target::local> l_D,
+            const int iW,
+            const int iH,
+            const int p
+        ): d_Dst{d_D}, d_Src{d_S}, d_Kernel{d_K}, imageW{iW}, imageH{iH}, pitch{p}, l_Data{l_D} {}
+    
+    private:
+            float* __restrict__ d_Dst;          // buffer<float,1> &d_Dst,
+            const float *__restrict__ d_Src;    // buffer<float,1> &d_Src,
+            const float *__restrict__ d_Kernel; // buffer<float,1> &d_Kernel,
+            accessor<float, 2, sycl_read_write, access::target::local> l_Data;
+            const int imageW;
+            const int imageH;
+            const int pitch;
+
+    public:
+    
+    void operator()(nd_item<2> item) const {
+        int gidX = item.get_group(1); 
+        int gidY = item.get_group(0); 
+        int lidX = item.get_local_id(1); 
+        int lidY = item.get_local_id(0); 
+
+        //Offset to the left halo edge
+        const int baseX = (gidX * ROWS_RESULT_STEPS - ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X + lidX;
+        const int baseY = gidY * ROWS_BLOCKDIM_Y + lidY;
+
+        const float* __restrict__ d_Src_new = d_Src + baseY * pitch + baseX;
+        float* __restrict__ d_Dst_new = d_Dst + baseY * pitch + baseX;
+
+        // Load main data
+        for (int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++) {
+          l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X] = d_Src_new[i * ROWS_BLOCKDIM_X];
+        }
+
+        // Load left halo
+        for (int i = 0; i < ROWS_HALO_STEPS; i++)
+          l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X] =
+              (baseX + i * ROWS_BLOCKDIM_X >= 0) ? d_Src_new[i * ROWS_BLOCKDIM_X] : 0;
+
+        // Load right halo
+        for (int i = ROWS_HALO_STEPS + ROWS_RESULT_STEPS;
+             i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS + ROWS_HALO_STEPS; i++)
+          l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X] =
+              (baseX + i * ROWS_BLOCKDIM_X < imageW) ? d_Src_new[i * ROWS_BLOCKDIM_X]
+                                                     : 0;
+
+        // Compute and store results
+        item.barrier(access::fence_space::local_space);
+        for (int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++) {
+          float sum = 0;
+
+          for (int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++)
+            sum += d_Kernel[KERNEL_RADIUS - j] *
+                   l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X + j];
+
+          d_Dst_new[i * ROWS_BLOCKDIM_X] = sum;
+        }
+    }
+};
+
 void convolutionRows(
     queue &q,
-    float *__restrict__ d_Dst,          // buffer<float,1> &d_Dst,
+    float* __restrict__ d_Dst,          // buffer<float,1> &d_Dst,
     const float *__restrict__ d_Src,    // buffer<float,1> &d_Src,
     const float *__restrict__ d_Kernel, // buffer<float,1> &d_Kernel,
     const int imageW,
@@ -39,48 +112,11 @@ void convolutionRows(
     range<2> lws(ROWS_BLOCKDIM_Y, ROWS_BLOCKDIM_X);
     range<2> gws(imageH, imageW / ROWS_RESULT_STEPS);
 
-    q.submit([&](handler &cgh)
-             {
+    q.submit([&](handler &cgh) {
       accessor<float, 2, sycl_read_write, access::target::local> 
       l_Data({ROWS_BLOCKDIM_Y, (ROWS_RESULT_STEPS + 2 * ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X}, cgh);
-
-    cgh.parallel_for<class conv_rows>(nd_range<2>(gws,lws), [=] (nd_item<2> item) {
-        int gidX = item.get_group(1); 
-        int gidY = item.get_group(0); 
-        int lidX = item.get_local_id(1); 
-        int lidY = item.get_local_id(0); 
-
-        //Offset to the left halo edge
-        const int baseX = (gidX * ROWS_RESULT_STEPS - ROWS_HALO_STEPS) * ROWS_BLOCKDIM_X + lidX;
-        const int baseY = gidY * ROWS_BLOCKDIM_Y + lidY;
-
-        const float* __restrict__ d_Src_new = d_Src + baseY * pitch + baseX;
-        float* __restrict__ d_Dst_new = d_Dst + baseY * pitch + baseX;
-        
-        //Load main data
-        for(int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++) {
-            l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X] = d_Src_new[i * ROWS_BLOCKDIM_X];
-        }
-
-        //Load left halo
-        for(int i = 0; i < ROWS_HALO_STEPS; i++)
-            l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X]  = (baseX + i * ROWS_BLOCKDIM_X >= 0) ? d_Src_new[i * ROWS_BLOCKDIM_X] : 0;
-
-        //Load right halo
-        for(int i = ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS + ROWS_HALO_STEPS; i++)
-            l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X]  = (baseX + i * ROWS_BLOCKDIM_X < imageW) ? d_Src_new[i * ROWS_BLOCKDIM_X] : 0;
-
-        //Compute and store results
-        item.barrier(access::fence_space::local_space);
-        for(int i = ROWS_HALO_STEPS; i < ROWS_HALO_STEPS + ROWS_RESULT_STEPS; i++){
-            float sum = 0;
-
-            for(int j = -KERNEL_RADIUS; j <= KERNEL_RADIUS; j++)
-                sum += d_Kernel[KERNEL_RADIUS - j] * l_Data[lidY][lidX + i * ROWS_BLOCKDIM_X + j];
-
-            d_Dst_new[i * ROWS_BLOCKDIM_X] = sum;
-        }
-      }); });
+      cgh.parallel_for<class conv_rows>(nd_range<2>(gws,lws), ConvRows{d_Dst, d_Src, d_Kernel, l_Data, imageW, imageH, pitch});
+    });
 }
 
 void convolutionColumns(
@@ -98,6 +134,7 @@ void convolutionColumns(
 
     range<2> lws(COLUMNS_BLOCKDIM_Y, COLUMNS_BLOCKDIM_X);
     range<2> gws(imageH / COLUMNS_RESULT_STEPS, imageW);
+
 
     q.submit([&](handler &cgh)
              {
@@ -140,5 +177,6 @@ void convolutionColumns(
 
             d_Dst_new[i * COLUMNS_BLOCKDIM_Y * pitch] = sum;
         }
-      }); });
+      });
+    });
 }
